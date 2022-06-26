29a36aadbee7aa44e966229eb2a7f5f9dff22f71
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-06-26 23:25:20 +0900

    氏名の振り分け完成。

--- csv/hirau.py
+++ csv/hirau.py
@@ -106,67 +106,49 @@ for org_file in org_files:
     tmp_arr = []
     for pick, puse in zip(pick_idx, puse_idx):
         sf = [row for row in df['Stuff'].iloc[pick:puse]]
+        # 「・」を持つ文字列を整理する。
+        df['Stuff'] = df['Stuff'].str.replace(pat='\s?・\s?', repl='▼')
         tmp_arr.append('　'.join(sf))
     df2['担当'] = pd.Series(tmp_arr)
-    # 「・」を持つ文字列を整理する。
-    df2['担当'] = df2['担当'].str.replace(pat='\s?・\s?', repl='▼')
     df2['担当'] = df2['担当'].str.replace(pat='\s?〓\s?', repl='')
     df2['担当'] = df2['担当'].str.strip()
-    # for idx, name in df2['担当'].iteritems():
-    #     name = name.split()
-    #     if len(name) == 2:
-    #         name.insert(0, '')
-    #         name.insert(1, '')
-    #         name.insert(2, '')
-    #         df2['担当'].iloc[idx] = name
-    #     elif len(name) == 3:
-    #         name.insert(4, '')
-    #         name.insert(5, '')
-    #         df2['担当'].iloc[idx] = name
-    #     else:
-    #         continue
-    # val = []
-    # for item in df2['担当']:
-    #     val.append(item)
-    # print(type(val))
+
+    for idx, name in df2['担当'].iteritems():
+        name = name.split()
+        if len(name) == 2:
+            name.insert(0, '〓')
+            name.insert(1, '〓')
+            name.insert(2, '〓')
+            df2['担当'].iloc[idx] = name
+        elif len(name) == 3:
+            name.insert(4, '〓')
+            name.insert(5, '〓')
+            df2['担当'].iloc[idx] = name
+        else:
+            df2['担当'].iloc[idx] = ['〓', '〓', '〓', '〓', '〓']
+
+    tmp_arr = []
+    for val in df2['担当']:
+        tmp_arr.append(val)
+    df3 = pd.DataFrame(tmp_arr)
+    df3 = df3.drop(columns = df3.columns[5:])
+    df3.columns = ['役職', '氏（役職）', '名（役職）', '氏', '名']
+    df3['氏名（役職）'] = df3['氏（役職）'] + "　" + df3['名（役職）']
+    df3['氏名（役職）'] = [ntzstr.name4justify(name) for name in df3['氏名（役職）']]
+    df3['氏名'] = df3['氏'] + "　" + df3['名']
+    df3['氏名'] = [ntzstr.name4justify(name) for name in df3['氏名']]
+    # ゲタを外す。
+    for col, ser in df3[['役職', '氏名（役職）', '氏名']].iteritems():
+        df3[col] = df3[col].str.replace(pat='\s?〓\s?', repl='')
     
-    # # df3 = pd.DataFrame(val, columns = ['役職', '氏（役職）', '名（役職）', '氏', '名'])
-    # df3 = pd.DataFrame(val)
-    # df3 = df3.drop(columns = [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
-    # df3.columns = ['役職', '氏（役職）', '名（役職）', '氏', '名']
-
-
-    # tmp_df = pd.DataFrame([df3['氏（役職）'], df3['名（役職）']])
-    # for col, td in tmp_df.iterrows():
-    #     td.fillna("〓〓", inplace = True)
-    #     df[col] = td
-    # df3['氏名（役職）'] = df3['氏（役職）'] + "　" + df3['名（役職）']
-    # for i in df3['氏名（役職）']:
-    #     print(i)
-
-    #####################
-    # 役職付氏名
-    # 5文字揃え
-    # df['氏名（役職）'] = [ntzstr.name4justify(name) for name in df3['氏名（役職）']]
-    # print(df['氏名（役職）'])
-        
-
-    # # print(tmp_arr)
-    # # for arr in tmp_arr:
-    # #     print(arr)
-    # df3 = pd.DataFrame(tmp_arr, columns = ['役職', '氏（役職）', '名（役職）', '氏', '名'])
-    # for idx, data in df3['役職'].iteritems():
-    #     print(data)
-
-
-
+    df2 = pd.concat([df2, df3['役職'], df3['氏名（役職）'], df3['氏名']], axis = 1)
 
     ########## CSVに上書きして完成
     to_gen_file = os.path.join('./_gen', filename)
     df2.to_csv(to_gen_file,
         encoding = "utf-16",
         index = False,
-        columns = ['郵便', '住所','会社','担当'],
+        columns = ['郵便', '住所','会社', '役職', '氏名（役職）', '氏名'],
         sep = ',')
 
     # #####################
@@ -174,23 +156,3 @@ for org_file in org_files:
     # ### 検証をするときはこれらを外す。
     # os.remove(org_file)
     # os.remove(tmp_file)
-
-# df2_stuff = df2['担当'].str.split(pat = '\W', n = 2, expand = True)
-# for idx, stuff in df2_stuff.iteritems():
-#     if len(stuff) == 2:
-#         print(f"{idx}, {stuff} yes, this is 2")
-#     else:
-#         print(f"{idx}, {stuff} yes, this is 3")
-# df3 = pd.DataFrame(df2_stuff, columns = ['first', 'second', 'third'])
-# df2_stuff.columns = ['first', 'second', 'third']
-# for col, arr in df3.iteritems():
-#     print(df)
-    # print(arr[first], arr[second], arr[third])
-
-# print(type(df2_stuff))
-# for idx, n in df2_stuff.iteritems():
-#     print(idx, n)
-# df2_stuff['氏'] = df2_stuff['氏'].str.strip()
-# df2_stuff['名'] = df2_stuff['名'].str.strip()
-# # ファイル名など変換用。このコード内で使用するためのもの。
-# df['氏名'] = df2_stuff['氏'] + "　" + df2_stuff['名']
